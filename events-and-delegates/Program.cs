034aed27be3cb921b620c6b7359fbc1f243d7ddb
Antonio Johnson <antonio@jtigernova.com>
2020-10-02 10:07:46 -0500

    add event that follows the standard signature for a .NET event delegate

--- events-and-delegates/Program.cs
+++ events-and-delegates/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static grasp.events_and_delegates.UsingEvents;
 using static System.Console;
 
 namespace grasp.events_and_delegates
@@ -11,7 +12,7 @@ namespace grasp.events_and_delegates
 
             WriteLine("\n\n---\n\n\n");
 
-            DoEvents();            
+            DoEvents();
         }
 
         private static void DoEvents()
@@ -25,13 +26,15 @@ namespace grasp.events_and_delegates
 
             WriteLine("\n\n");
 
-            EventHandler<float> onPassEventHandler = (object sender, float speed) => {
+            EventHandler<float> onPassEventHandler = (object sender, float speed) =>
+            {
                 WriteLine($"EVENT-1... A pass was thrown at a speed of {speed}m/s");
             };
 
             //let's hook up some events
             usingEvents.Pass += OnPass;
             usingEvents.Pass += onPassEventHandler;
+            usingEvents.PassDetailed += OnPassDetailed;
 
             //events are triggered
             usingEvents.PassTheBall(ballSpeed);
@@ -58,6 +61,11 @@ namespace grasp.events_and_delegates
         private static void OnPass(object sender, float speed)
         {
             WriteLine($"EVENT-2... A pass was thrown at a speed of {speed}m/s");
+        }
+
+        private static void OnPassDetailed(object sender, PassArgs args)
+        {
+            WriteLine($"EVENT-3... A detailed pass was thrown at a speed of {args.BallSpeed}m/s");
         }
 
         static void AnotherPrintMessage(string message)
--- events-and-delegates/UsingEvents.cs
+++ events-and-delegates/UsingEvents.cs
@@ -7,11 +7,27 @@ namespace grasp.events_and_delegates
     {
         public event EventHandler<float> Pass;
 
+        /// <summary>
+        /// Event handler that follows the standard signature for a .NET event delegate
+        /// </summary>
+        public event EventHandler<PassArgs> PassDetailed;
+
         public void PassTheBall(float ballSpeed)
         {
             WriteLine("Ball was passed. Triggering events... If any exists");
 
             Pass?.Invoke(this, ballSpeed);
+            PassDetailed?.Invoke(this, new PassArgs(ballSpeed));
+        }
+
+        public class PassArgs : EventArgs
+        {
+            public float BallSpeed { get; }
+
+            public PassArgs(float ballSpeed)
+            {
+                BallSpeed = ballSpeed;
+            }
         }
     }
 }
